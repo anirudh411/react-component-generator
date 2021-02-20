b5e3b74994d87aceecef20cbe77babe2820e8423
anirudh <anirudh.chauhan.142@gmail.com>
2021-02-20 20:45:21 +0530

    added global install command

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,12 @@
+#!/usr/bin/env node
 const inquirer = require("../lib/inquirer");
+const files = require("../lib/files");
+
 const fs = require("fs");
 const run = async () => {
   const { path, component } = await inquirer.askCompoentQuestions();
   if (path && component) {
-    fs.mkdirSync(path);
-    fs.writeFileSync(
-      `${path}/${component}.js`,
-      `import * as React from 'react';\n\nconst ${component} = () => null\n\n export default ${component}`
-    );
-    fs.writeFileSync(
-      `${path}/index.js`,
-      `import ${component} from './${component}.js';\n\n export default ${component}`
-    );
+    files.writeComponent({componentName:component,path})
   }
 };
 run();
