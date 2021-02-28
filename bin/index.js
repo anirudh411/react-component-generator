e53025c26a84612278de025da0f37b787bbbb7a9
anirudh <anirudh.chauhan.142@gmail.com>
2021-02-28 09:42:24 +0530

    added status messages

--- bin/index.js
+++ bin/index.js
@@ -2,11 +2,10 @@
 const inquirer = require("../lib/inquirer");
 const files = require("../lib/files");
 
-const fs = require("fs");
 const run = async () => {
-  const { path, component } = await inquirer.askCompoentQuestions();
+  const { path, component } = await inquirer.askComponentQuestions();
   if (path && component) {
-    files.writeComponent({componentName:component,path})
+    files.writeComponent({ componentName: component, pathName: path });
   }
 };
 run();
